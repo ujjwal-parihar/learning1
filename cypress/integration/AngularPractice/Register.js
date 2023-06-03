f7056308ca0b533ab30a4d8c24a90caf418b3be1
Ujjwal Parihar <ujjwalparihar@Ujjwals-MacBook-Air.local>
2023-06-03 15:21:06 +0530

    This is udate in Register

--- cypress/integration/AngularPractice/Register.js
+++ cypress/integration/AngularPractice/Register.js
@@ -11,8 +11,29 @@ describe('TS1: angularpractice Page',function(){
         cy.title().should('include', 'nopCommerce demo store')
     });
 
-        
+    it('TC2: Verify availability of Register link', function(){
+        cy.get('a.ico-register').should("have.attr", "href", "/register?returnUrl=%2F");
+        //cy.get('a.ico-register').click()
+    }); 
+    it('TC3: Verify that on clicking Register link, user redirects to Registration page. Verify the link', function(){
+        cy.get('a.ico-register').click();
+        cy.url().should('include', '/register?returnUrl=%2F')
 
+    }); 
+    it('TC4: Verify the page title', function(){
+        cy.get('a.ico-register').click();
+        cy.get('div.page-title h1').then(function(txt){
+            const txts= txt.text()
+            expect(txts).have.eql('Register')
+        
+        })
+        
+    }); 
+    it('TC5: Verify that Gender: Male and Female are not selected', function(){
+        cy.get('a.ico-register').click();
+        cy.get('#gender-male').should('not.be.selected')
+        cy.get('#gender-female').should('not.be.selected')
+    });
     }) 
 
 
